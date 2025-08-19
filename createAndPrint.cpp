6f3504275b6268803bcb0ff44453d498d33f3413
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-19 20:14:19 +0600

    Creating a string and Accessing string.

---
+++ createAndPrint.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // Creating a string
+    string greeting = "Welcome to GfG!";
+    
+    // Accessing string
+    cout << greeting;
+
+    return 0;
+}
