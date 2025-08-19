8d3e701560fe2b01bce339d3d59cbd00d8f84971
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-19 20:17:39 +0600

    The individual characters of the strings can also be accessed using their position (or index) like arrays with [] square brackets.

---
+++ accessByIndex.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "Geek";
+    
+    // Accessing 3rd character
+    cout << str[2] << endl;
+    
+    // Accessing first character
+    cout << str[0];
+
+    return 0;
+}
